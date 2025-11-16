d561f09192a93928fd5d54da5da5e78eee3e6fe9
Mauro Cabrera <maurocabricci17@gmail.com>
2025-11-16 19:09:36 -0300

    javascript logra cambiar estilo

--- html-css/script.js
+++ html-css/script.js
@@ -4,11 +4,9 @@ const boton = document.querySelector('.menu-button');
 
 function menuButtonClick() {
     navegador.classList.toggle('mobile-menu');
+
 }
 
-function pingo(){
-    console.log("sexoooo");
-}
 
 // Ejemplo de cómo asignar la función a un botón con id="miBoton"
-document.getElementById(boton).addEventListener('click', pingo);
+boton.addEventListener('click', menuButtonClick);
